a26c7b87691d9ca44de74e6a8da42c4ec3785ddd
Yasir Akhlaque <akhlaqueyasir28@gmail.com>
2025-10-29 13:15:09 +0530

    Updated app.js with new routes

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import corsMiddleware from "./middlewares/cors.middleware.js";
 import authRoutes from './routes/auth.routes.js';
+import questionPaperRoutes from './routes/questionpaper.routes.js';
+import subjectRoutes from './routes/subject.routes.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -10,5 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(corsMiddleware);
 app.use('/api/auth', authRoutes);
+app.use('/api/questionpapers', questionPaperRoutes);
+app.use('/api/subjects', subjectRoutes);
 
 export default app;
